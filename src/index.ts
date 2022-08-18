58ce47423c70173eea0f176c89ae03e0a506bc2c
James Hush <james@daily.co>
2022-08-18 17:13:38 +0800

    Stub out methods

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
-import { Session, TokenOptions } from "opentok";
+/* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Archive,
+  ArchiveOptions,
+  Broadcast,
+  BroadcastLayoutType,
+  BroadcastOptions,
+  BroadcastStopResponse,
+  DialOptions,
+  ListArchivesOptions,
+  ListBroadcastsOptions,
+  PatchStream,
+  Session,
+  SignalOptions,
+  SipInterconnect,
+  Stream,
+  StreamId,
+  Token,
+  TokenOptions,
+} from "opentok";
 import { createRoom, getMeetingToken, Domain, getDomainID } from "./daily";
 
+function notImplemented(): never {
+  throw new Error("Method not implemented.");
+}
 type MediaMode = "routed" | "relayed";
 type ArchiveMode = "always" | "manual";
 
@@ -25,6 +48,188 @@ class OpenTokClass {
     this.getDomainID().then((id) => {
       this.domainID = id;
     });
+  }
+
+  public deleteArchive(
+    archiveId: string,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public dial(
+    sessionId: string,
+    token: Token,
+    sipUri: string,
+    options: DialOptions,
+    callback: (error: Error | null, sipInterconnect: SipInterconnect) => void
+  ): void {
+    notImplemented();
+  }
+
+  public forceDisconnect(
+    sessionId: string,
+    connectionId: string,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public getArchive(
+    archiveId: string,
+    callback: (error: Error | null, archive?: Archive) => void
+  ): void {
+    notImplemented();
+  }
+
+  public getBroadcast(
+    broadcastId: string,
+    callback: (error: Error | null, broadcast?: Broadcast) => void
+  ): void {
+    notImplemented();
+  }
+
+  public getStream(
+    sessionId: string,
+    options: StreamId,
+    callback: (error: Error | null, stream?: Stream) => void
+  ): void {
+    notImplemented();
+  }
+
+  public listArchives(
+    options: ListArchivesOptions,
+    callback: (
+      error: Error | null,
+      archives?: Archive[],
+      totalCount?: number
+    ) => void
+  ): void {
+    notImplemented();
+  }
+
+  public listBroadcasts(
+    options: ListBroadcastsOptions,
+    callback: (error: Error | null, broadcasts?: Broadcast[]) => void
+  ): void {
+    notImplemented();
+  }
+
+  public listStreams(
+    sessionId: string,
+    callback: (error: Error | null, streams?: Stream[]) => void
+  ): void {
+    notImplemented();
+  }
+
+  public addArchiveStream(
+    archiveId: string,
+    streamId: string,
+    options: PatchStream,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public removeArchiveStream(
+    archiveId: string,
+    streamId: string,
+    options: PatchStream,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public addBroadcastStream(
+    broadcastId: string,
+    streamId: string,
+    options: PatchStream,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public removeBroadcastStream(
+    broadcastId: string,
+    streamId: string,
+    options: PatchStream,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public playDTMF(
+    sessionId: string,
+    connectionId: string,
+    digits: string,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public setArchiveLayout(
+    archiveId: string,
+    type: BroadcastLayoutType | "custom",
+    stylesheet: string | null,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public setBroadcastLayout(
+    broadcastId: string,
+    type: BroadcastLayoutType | "custom",
+    stylesheet: string | null,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public setStreamClassLists(
+    sessionId: string,
+    classListArray: ReadonlyArray<{ id: string; layoutClassList: string[] }>,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public signal(
+    sessionId: string,
+    connectionId: string | null,
+    data: SignalOptions,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public startArchive(
+    sessionId: string,
+    options: ArchiveOptions,
+    callback: (error: Error | null, archive?: Archive) => void
+  ): void {
+    notImplemented();
+  }
+
+  public startBroadcast(
+    sessionId: string,
+    options: BroadcastOptions,
+    callback: (error: Error | null, broadcast: Broadcast) => void
+  ): void {
+    notImplemented();
+  }
+
+  public stopArchive(
+    archiveId: string,
+    callback: (error: Error | null, archive?: Archive) => void
+  ): void {
+    notImplemented();
+  }
+
+  public stopBroadcast(
+    broadcastId: string,
+    callback: (error: Error | null, broadcast: BroadcastStopResponse) => void
+  ): void {
+    notImplemented();
   }
 
   // getDomainID() retrieves a Daily domain ID
