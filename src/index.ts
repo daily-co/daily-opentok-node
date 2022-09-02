eaede8e694cd86e99f07fc021939a4c30207fc6a
James Hush <james@daily.co>
2022-09-02 15:58:35 +0800

    Stub out everything (#2)
    * Save progress from liza changes
    
    * Stub out methods
    
    * Code
    
    * Make domainId a seperate call

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
-import { Session, TokenOptions } from "opentok";
+/* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Archive,
+  ArchiveOptions,
+  Broadcast,
+  BroadcastLayoutType,
+  BroadcastOptions,
+  BroadcastStopResponse,
+  DialOptions,
+  ListArchivesOptions,
+  ListBroadcastsOptions,
+  PatchStream,
+  Session,
+  SignalOptions,
+  SipInterconnect,
+  Stream,
+  StreamId,
+  Token,
+  TokenOptions,
+} from "opentok";
 import { createRoom, getMeetingToken, Domain, getDomainID } from "./daily";
 
+function notImplemented(): never {
+  throw new Error("Method not implemented.");
+}
 type MediaMode = "routed" | "relayed";
 type ArchiveMode = "always" | "manual";
 
@@ -15,16 +38,204 @@ type OTOptions = {
 // What we'll call after a session is created
 type Callback = (error: Error | null, session?: Session) => void;
 
-export default class OpenTok {
+class OpenTokClass {
   private readonly apiKey: string;
+
+  private domainID: string;
 
   constructor(apiKey: string, _apiSecret: string) {
     this.apiKey = apiKey;
+    this.domainID = "";
+  }
+
+  public deleteArchive(
+    archiveId: string,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public dial(
+    sessionId: string,
+    token: Token,
+    sipUri: string,
+    options: DialOptions,
+    callback: (error: Error | null, sipInterconnect: SipInterconnect) => void
+  ): void {
+    notImplemented();
+  }
+
+  public forceDisconnect(
+    sessionId: string,
+    connectionId: string,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public getArchive(
+    archiveId: string,
+    callback: (error: Error | null, archive?: Archive) => void
+  ): void {
+    notImplemented();
+  }
+
+  public getBroadcast(
+    broadcastId: string,
+    callback: (error: Error | null, broadcast?: Broadcast) => void
+  ): void {
+    notImplemented();
+  }
+
+  public getStream(
+    sessionId: string,
+    options: StreamId,
+    callback: (error: Error | null, stream?: Stream) => void
+  ): void {
+    notImplemented();
+  }
+
+  public listArchives(
+    options: ListArchivesOptions,
+    callback: (
+      error: Error | null,
+      archives?: Archive[],
+      totalCount?: number
+    ) => void
+  ): void {
+    notImplemented();
+  }
+
+  public listBroadcasts(
+    options: ListBroadcastsOptions,
+    callback: (error: Error | null, broadcasts?: Broadcast[]) => void
+  ): void {
+    notImplemented();
+  }
+
+  public listStreams(
+    sessionId: string,
+    callback: (error: Error | null, streams?: Stream[]) => void
+  ): void {
+    notImplemented();
+  }
+
+  public addArchiveStream(
+    archiveId: string,
+    streamId: string,
+    options: PatchStream,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public removeArchiveStream(
+    archiveId: string,
+    streamId: string,
+    options: PatchStream,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public addBroadcastStream(
+    broadcastId: string,
+    streamId: string,
+    options: PatchStream,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public removeBroadcastStream(
+    broadcastId: string,
+    streamId: string,
+    options: PatchStream,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public playDTMF(
+    sessionId: string,
+    connectionId: string,
+    digits: string,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public setArchiveLayout(
+    archiveId: string,
+    type: BroadcastLayoutType | "custom",
+    stylesheet: string | null,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public setBroadcastLayout(
+    broadcastId: string,
+    type: BroadcastLayoutType | "custom",
+    stylesheet: string | null,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public setStreamClassLists(
+    sessionId: string,
+    classListArray: ReadonlyArray<{ id: string; layoutClassList: string[] }>,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public signal(
+    sessionId: string,
+    connectionId: string | null,
+    data: SignalOptions,
+    callback: (error: Error | null) => void
+  ): void {
+    notImplemented();
+  }
+
+  public startArchive(
+    sessionId: string,
+    options: ArchiveOptions,
+    callback: (error: Error | null, archive?: Archive) => void
+  ): void {
+    notImplemented();
+  }
+
+  public startBroadcast(
+    sessionId: string,
+    options: BroadcastOptions,
+    callback: (error: Error | null, broadcast: Broadcast) => void
+  ): void {
+    notImplemented();
+  }
+
+  public stopArchive(
+    archiveId: string,
+    callback: (error: Error | null, archive?: Archive) => void
+  ): void {
+    notImplemented();
+  }
+
+  public stopBroadcast(
+    broadcastId: string,
+    callback: (error: Error | null, broadcast: BroadcastStopResponse) => void
+  ): void {
+    notImplemented();
   }
 
   // getDomainID() retrieves a Daily domain ID
-  getDomainID(): Promise<string> {
-    return getDomainID(this.apiKey).then((id) => id);
+  public getDomainID(): Promise<string> {
+    return getDomainID(this.apiKey).then((id) => {
+      this.domainID = id;
+      return id;
+    });
   }
 
   // createSession() creates a Daily room and wrangles it
@@ -32,8 +243,11 @@ export default class OpenTok {
   createSession(_opts: OTOptions, callback: Callback) {
     createRoom(this.apiKey)
       .then((room) => {
-        const session = <Session>{
+        const session = {
           sessionId: room.url,
+          mediaMode: "routed",
+          archiveMode: "manual",
+          ot: this,
         };
         callback(null, session);
       })
@@ -43,10 +257,19 @@ export default class OpenTok {
   }
 
   // generateToken() returns a self-signed Daily meeting token
-  generateToken(sessionID: string, options?: TokenOptions & Domain): string {
-    if (!options?.domainID) {
-      throw new Error("expecting Daily domain ID in options");
+  generateToken(sessionID: string, options?: TokenOptions): string {
+    if (!this.domainID) {
+      throw new Error(
+        "Daily domain ID is missing. Did you call getDomainID() first?"
+      );
     }
-    return getMeetingToken(this.apiKey, sessionID, options);
+    return getMeetingToken(this.apiKey, sessionID, {
+      ...options,
+      domainID: this.domainID,
+    });
   }
+}
+
+export default function OpenTok(apiKey: string, domainID: string, env: {}) {
+  return new OpenTokClass(apiKey, domainID);
 }
