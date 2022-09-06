b7373205dbf7dbe2bfa7e6e8406d82dc3cb8cac6
Liza <65890040+lazeratops@users.noreply.github.com>
2022-09-06 11:55:28 +0200

    WIP OpenTok node shim (#1)

---
+++ src/daily.ts
@@ -0,0 +1,138 @@
+import axios from "axios";
+import { TokenOptions } from "opentok";
+import * as jwt from "jsonwebtoken";
+
+// The data we'll expect to get from Daily on room creation.
+// Daily actually returns much more data, but these are the only
+// properties we'll be using.
+interface DailyRoomData {
+  id: string;
+  name: string;
+  url: string;
+}
+
+export interface Domain {
+  domainID: string;
+}
+
+export interface DailyTokenPayload {
+  r: string;
+  d: string;
+  exp: number;
+  o: boolean;
+  iat: number;
+}
+
+const dailyAPIDomain = "daily.co";
+const dailyAPIURL = `https://api.${dailyAPIDomain}/v1`;
+
+export async function createRoom(apiKey: string): Promise<DailyRoomData> {
+  const req = {
+    // TODO: find out if OT sessions have a default
+    // expiry time or any other default options that map
+    // to Daily rooms.
+    properties: {
+      start_audio_off: false,
+      start_video_off: false,
+    },
+  };
+
+  // Prepare our headers, containing our Daily API key
+  const headers = {
+    Authorization: `Bearer ${apiKey}`,
+    "Content-Type": "application/json",
+  };
+
+  const url = `${dailyAPIURL}/rooms/`;
+  const data = JSON.stringify(req);
+  const roomErrMsg = "failed to create room";
+  // We won't do any error logging here, delegating logging
+  // and other error handling to the judgement of the caller.
+  const res = await axios.post(url, data, { headers }).catch((error) => {
+    throw new Error(`${roomErrMsg}: ${error})`);
+  });
+
+  if (res.status !== 200 || !res.data) {
+    throw new Error(roomErrMsg);
+  }
+  // Cast Daily's response to our room data interface.
+  const roomData = <DailyRoomData>res.data;
+  return roomData;
+}
+
+function getRoomName(roomURL: string): string {
+  const url = new URL(roomURL);
+  const roomName = url.pathname.replace("/", "");
+  return roomName;
+}
+
+export function getDomainID(apiKey: string): Promise<string> {
+  const headers = {
+    Authorization: `Bearer ${apiKey}`,
+    "Content-Type": "application/json",
+  };
+
+  const url = dailyAPIURL;
+  const errMsg = "failed to get domain ID";
+  return axios
+    .get(url, { headers })
+    .then((res) => {
+      if (res.status !== 200 || !res.data) {
+        throw new Error(errMsg);
+      }
+      return res.data.domain_id;
+    })
+    .catch((error) => {
+      throw new Error(`${errMsg}: ${error})`);
+    });
+}
+
+// getMeetingToken() obtains a meeting token for a room from Daily.
+export function getMeetingToken(
+  apiKey: string,
+  roomURL: string,
+  opts: TokenOptions & Domain
+): string {
+  const now = Math.floor(Date.now() / 1000);
+  const defaultExp = now + 3600;
+
+  const roomName = getRoomName(roomURL);
+  const { domainID } = opts;
+  if (!domainID) {
+    throw new Error("options must contain domain ID");
+  }
+
+  // We are going to self-sign these, removing
+  // need for a round-trip.
+  const payload = <DailyTokenPayload>{
+    r: roomName,
+    d: domainID,
+    exp: defaultExp,
+    o: false,
+    iat: now,
+  };
+
+  const exp = opts?.expireTime;
+  if (exp) {
+    if (new Date(exp).getTime() <= 0) {
+      throw new Error(
+        `invalid expire time provided. Expiry must be a UNIX time stamp: ${exp}`
+      );
+    }
+    payload.exp = exp;
+  }
+
+  const role = opts?.role;
+  // We're going to treat a "moderator" as the equivalent of
+  // a Daily room "owner"
+  if (role === "moderator") {
+    payload.o = true;
+  }
+  const jp = JSON.stringify(payload);
+  try {
+    const token = jwt.sign(jp, apiKey);
+    return token;
+  } catch (e) {
+    throw new Error(`failed to create self-signed JWT: ${e.toString()}`);
+  }
+}
---
+++ src/tests/daily.test.ts
@@ -0,0 +1,62 @@
+import * as jwt from "jsonwebtoken";
+import { TokenOptions } from "opentok";
+import { DailyTokenPayload, Domain, getMeetingToken } from "../daily";
+
+describe("Daily meeting token retrieval tests", () => {
+  test("Success with default timestamp", () => {
+    const secret = "very-very-secret";
+    const roomURL = "https://mydomain.daily.co/roomname";
+
+    // Freeze the
+    const now = Date.now();
+    jest.spyOn(Date, "now").mockImplementation(() => now);
+
+    const domainID = "domainID";
+    const wantPayload = <DailyTokenPayload>{
+      r: "roomname",
+      d: domainID,
+      // exp should be the default, 24 hours
+      exp: Math.floor(now / 1000) + 3600,
+      o: false,
+      iat: Math.floor(now / 1000),
+    };
+
+    const opts = {
+      domainID,
+    };
+
+    const gotToken = getMeetingToken(secret, roomURL, opts);
+    const gotPayload = <DailyTokenPayload>jwt.decode(gotToken);
+    expect(gotPayload).toStrictEqual(wantPayload);
+  });
+  test("Success with explicit exp and role", () => {
+    const secret = "very-very-secret";
+    const roomURL = "https://mydomain.daily.co/roomname";
+
+    // Freeze time
+    const now = Date.now();
+    jest.spyOn(Date, "now").mockImplementation(() => now);
+
+    const wantExp = now + 100;
+
+    const domainID = "domain-id";
+    const wantPayload = <DailyTokenPayload>{
+      r: "roomname",
+      d: domainID,
+      // exp should be the default, 24 hours
+      exp: wantExp,
+      o: true,
+      iat: Math.floor(now / 1000),
+    };
+
+    const opts = <TokenOptions & Domain>{
+      domainID,
+      expireTime: wantExp,
+      role: "moderator",
+    };
+
+    const gotToken = getMeetingToken(secret, roomURL, opts);
+    const gotPayload = <DailyTokenPayload>jwt.decode(gotToken);
+    expect(gotPayload).toStrictEqual(wantPayload);
+  });
+});
